3206f507a1a40858bb9e3b8422cce82e724b9841
Arjun <arjunbedi1411@gmail.com>
2022-04-02 17:03:22 +0530

    <AB> Project initial Setup for TshirtOrderPlacingPortal (Added React app, .net core web Api, Database dump scripts)

---
+++ TshirtOrderPlacingPortal.Core/TshirtOrderPlacingPortal.Core/TshirtOrderPlacingPortal.Core/Controllers/TshirtController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using TshirtOrderPlacingPortal.DTO.Models;
+using TshirtOrderPlacingPortal.Infrastructure.Configuration.Contract;
+
+
+namespace TshirtOrderPlacingPortal.Core.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TshirtController : ControllerBase
+    {
+        
+     
+
+        public TshirtController()
+        {
+         
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+           
+            return Ok();
+        }
+
+ 
+    }
+}
